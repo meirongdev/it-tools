749c71f935f8bdea70f481ffaa2ec11fda013492
Meirong <meirongdev@gmail.com>
2026-02-19 18:13:07 +0800

    feat: remove UI Lib button

--- src/layouts/base.layout.vue
+++ src/layouts/base.layout.vue
@@ -87,12 +87,6 @@ const tools = computed<ToolCategory[]>(() => [
           </c-button>
         </c-tooltip>
 
-        <c-tooltip :tooltip="$t('home.uiLib')" position="bottom">
-          <c-button v-if="config.app.env === 'development'" to="/c-lib" circle variant="text" :aria-label="$t('home.uiLib')">
-            <icon-mdi:brush-variant text-20px />
-          </c-button>
-        </c-tooltip>
-
         <command-palette />
 
         <locale-selector v-if="!styleStore.isSmallScreen" />
